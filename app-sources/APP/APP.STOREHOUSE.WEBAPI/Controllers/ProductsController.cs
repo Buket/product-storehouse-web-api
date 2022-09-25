58d241ad966d8230643707cfc1d0b410d06e22df
Bulat Mukhametshin <m11e@yandex.ru>
2022-09-25 20:02:38 +0500

    Добавлены исходные файлы приложения

---
+++ app-sources/APP/APP.STOREHOUSE.WEBAPI/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using APP.STOREHOUSE.WEBAPI.Data;
+using APP.STOREHOUSE.WEBAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APP.STOREHOUSE.WEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<Product> Get([FromQuery] string productname)
+        {
+            StorehouseContext context = new StorehouseContext();
+            var result = context.Products.Where(x => x.Name.Contains(productname)).ToArray();
+            return result;
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] Product product)
+        {
+        }
+
+        // PUT api/<ProductsController>
+        [HttpPut()]
+        public void Put([FromBody] Product product)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id:guid}")]
+        public void Delete(Guid id)
+        {
+            StorehouseContext context = new StorehouseContext();
+            var product = context.Products.Find(id);
+            if (product != null)
+            {
+                context.Products.Remove(product);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ app-sources/APP/APP.STOREHOUSE.WEBAPI/Data/StorehouseContext.cs
@@ -0,0 +1,20 @@
+﻿using APP.STOREHOUSE.WEBAPI.Models;
+using System.Data.Entity;
+
+namespace APP.STOREHOUSE.WEBAPI.Data
+{
+    public class StorehouseContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public StorehouseContext()
+            : base(@"Data Source=DESKTOP-K1GCKQ5\MSSQLSERVER2017;Initial Catalog=TestDb;User ID=TestDbLogin;Password=TestDbDevelop1;")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
