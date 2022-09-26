7f49f4de7080525ca6387633963f28f54ddaa741
Bulat Mukhametshin <m11e@yandex.ru>
2022-09-26 12:44:28 +0500

    добавлен проект с тестами

---
+++ app-sources/APP/APP.STOREHOUSE.WEBAPI.TESTS/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APP.STOREHOUSE.WEBAPI.TESTS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
