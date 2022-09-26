f9238cb49267d522ce7b8fa2925313534c44b8b9
Bulat Mukhametshin <m11e@yandex.ru>
2022-09-26 15:06:21 +0500

    исправленв работа с транзакцией в ProductService
    Signed-off-by: Bulat Mukhametshin <m11e@yandex.ru>

--- app-sources/APP/APP.STOREHOUSE.WEBAPI/Services/ProductService.cs
+++ app-sources/APP/APP.STOREHOUSE.WEBAPI/Services/ProductService.cs
@@ -23,41 +23,35 @@ namespace APP.STOREHOUSE.WEBAPI.Services
 
         public Guid Create(Product product)
         {
-            using (context.Database.BeginTransaction())
+            using var transaction = context.Database.BeginTransaction(System.Data.IsolationLevel.Serializable);
+            if (product.Id == default)
             {
-                if (product.Id == default)
-                {
-                    product.Id = Guid.NewGuid();
-                }
-                context.Products.Add(product);
-                context.SaveChanges();
+                product.Id = Guid.NewGuid();
             }
+            context.Products.Add(product);
+            context.SaveChanges();
+            transaction.Commit();
 
             return product.Id;
         }
 
         public void Update(Product product)
         {
-            using (context.Database.BeginTransaction())
-            {
-                var storedProduct = context.Products.Find(product.Id) ?? throw new NotFoundException(Product.ProductName, product.Id);
-                context.Products.Remove(storedProduct);
-                context.Products.Add(product);
-                context.SaveChanges();
-            }
+            using var transaction = context.Database.BeginTransaction();
+            var storedProduct = context.Products.Find(product.Id) ?? throw new NotFoundException(Product.ProductName, product.Id);
+            context.Products.Remove(storedProduct);
+            context.Products.Add(product);
+            context.SaveChanges();
+            transaction.Commit();
         }
 
         public void Delete(Guid id)
         {
-            using (context.Database.BeginTransaction())
-            {
-                var product = context.Products.Find(id) ?? throw new NotFoundException(Product.ProductName, id);
-                if (product != null)
-                {
-                    context.Products.Remove(product);
-                    context.SaveChanges();
-                }
-            }
+            using var transaction = context.Database.BeginTransaction();
+            var product = context.Products.Find(id) ?? throw new NotFoundException(Product.ProductName, id);
+            context.Products.Remove(product);
+            context.SaveChanges();
+            transaction.Commit();
         }
     }
 }
